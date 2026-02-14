cd84eca2fbb464d5da14e4a1a93b1943f9c52dfa
João Victor Demarco <joaodemarco10@gmail.com>
2026-02-14 14:51:24 -0300

    initial project setup

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="min-h-svh">
+        <header class="p-6 border-b border-b-gray-300 flex items-center">
+            <p class="text-lg text-red">This is a simple layout</p>
+        </header>
+
+        <main>
+            <slot />
+        </main>
+    </div>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+
+defineOptions({
+    layout: AppLayout
+})
+</script>
+
+<template>
+<h1 class="text-2xl text-black font-sans">This is the home page</h1>
+</template>
